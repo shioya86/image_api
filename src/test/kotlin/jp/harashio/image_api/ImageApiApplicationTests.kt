0f70fdad404777abd9afee52355d535720a2a02f
shioya86 <admin@ml.harashio.jp>
2025-07-25 23:52:54 +0900

    initial commit

---
+++ src/test/kotlin/jp/harashio/image_api/ImageApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.harashio.image_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImageApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
